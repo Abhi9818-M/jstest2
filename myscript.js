e72dc804eb5e465e7a261049539d81478fd04d2e
Abhi9818-M <abhishekmishra64thps@gmail.com>
2023-08-20 22:36:37 +0530

    new add

---
+++ myscript.js
@@ -0,0 +1,28 @@
+function w(){
+    var audio = new Audio("song/sounds_tom-1.mp3");
+    audio.play();
+}
+function a(){
+    var audio = new Audio("song/sounds_tom-2.mp3");
+    audio.play();
+}
+function s(){
+    var audio = new Audio("song/sounds_tom-3.mp3");
+    audio.play();
+}
+function d(){
+    var audio = new Audio("song/sounds_tom-4.mp3");
+    audio.play();
+}
+function j(){
+    var audio = new Audio("song/sounds_snare.mp3");
+    audio.play();
+}
+function k(){
+    var audio = new Audio("song/sounds_crash.mp3");
+    audio.play();
+}
+function l(){
+    var audio = new Audio("song/sounds_kick-bass.mp3");
+    audio.play();
+}
